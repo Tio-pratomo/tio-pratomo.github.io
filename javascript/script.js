ffcc2a94d8fb793be76cb57029dbb98cce864b49
tio <sulistiopratomo@gmail.com>
2020-08-24 19:56:27 +0700

    add javascript file

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+function cvSupport() {
+    return alert(
+        `!!!!!!PERHATIAN!!!!!!
+
+     Berkas tersebut hanya di peruntukan untuk melamar kerja,berkas tersebut di tujukan agar perusahaan/divisi HRD dapat mencari lebih lanjut latar belakang, dan kemampuan pelamar. tidak di benarkan untuk memanipulasi, atau memanfaatkan berkas tersebut untuk kepentingan pribadi, karena melanggar hukum.terima kasih.`
+    );
+}
+
+document.querySelector('[target = "_blank"]').onclick = cvSupport;
