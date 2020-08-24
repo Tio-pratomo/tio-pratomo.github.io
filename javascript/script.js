bff832f8f0ec0eb758139ebdeaca85809f644931
tio <sulistiopratomo@gmail.com>
2020-08-24 20:02:48 +0700

    modified a little bit for alert

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,8 @@ function cvSupport() {
     return alert(
         `!!!!!!PERHATIAN!!!!!!
 
-     Berkas tersebut hanya di peruntukan untuk melamar kerja,berkas tersebut di tujukan agar perusahaan/divisi HRD dapat mencari lebih lanjut latar belakang, dan kemampuan pelamar. tidak di benarkan untuk memanipulasi, atau memanfaatkan berkas tersebut untuk kepentingan pribadi, karena melanggar hukum.terima kasih.`
+    Berkas tersebut hanya di peruntukan untuk melamar kerja,berkas tersebut di tujukan agar perusahaan/divisi HRD dapat mencari lebih lanjut latar belakang, dan kemampuan pelamar. tidak di benarkan untuk memanipulasi, atau memanfaatkan berkas tersebut untuk kepentingan pribadi, karena melanggar hukum.
+    terima kasih atas perhatiannya.`
     );
 }
 
