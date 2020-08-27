fed4c18002f41e25a2b8635a619f583341e49068
tio <sulistiopratomo@gmail.com>
2020-08-28 03:47:05 +0700

    change alert for CV support

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,11 @@
 function cvSupport() {
     return alert(
-        `!!!!!!PERHATIAN!!!!!!
-
-    Berkas tersebut hanya di peruntukan untuk melamar kerja,berkas tersebut di tujukan agar perusahaan/divisi HRD dapat mencari lebih lanjut latar belakang, dan kemampuan pelamar. tidak di benarkan untuk memanipulasi, atau memanfaatkan berkas tersebut untuk kepentingan pribadi, karena melanggar hukum.
+        `!!!!!!PERHATIAN!!!!!!\n \n
+    Berkas tersebut hanya di peruntukan untuk melamar kerja. \n
+    Berkas tersebut di tujukan agar perusahaan/divisi HRD dapat mencari lebih lanjut \n
+    latar belakang, dan kemampuan pelamar.\n
+    Tidak di benarkan untuk memanipulasi, atau memanfaatkan berkas tersebut untuk kepentingan pribadi,\n 
+    karena melanggar hukum.\n
     terima kasih atas perhatiannya.`
     );
 }
