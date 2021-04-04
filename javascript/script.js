25b78f6095e1e5019d85ee508e1ea63f07148a80
tio <sulistiopratomo@gmail.com>
2021-04-04 16:46:46 +0700

    mengubah link print as pdf pada javascript

--- javascript/script.js
+++ javascript/script.js
@@ -25,3 +25,10 @@ function copyrightYear() {
 
 document.querySelector('li.nav-link').onclick = cvSupport;
 document.querySelector('footer span').innerHTML = copyrightYear();
+
+// membuat link untuk print versi pdf
+const pdfVersion = document.querySelector('.nav-item a[href="#"]');
+pdfVersion.addEventListener('click', function (event) {
+    location.href = 'CV_1.pdf';
+    event.preventDefault();
+});
