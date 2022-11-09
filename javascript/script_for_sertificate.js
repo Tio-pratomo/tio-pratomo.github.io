d20140c60a05ff2087078195464a770f9e97f6b5
tio <sulistiopratomo@gmail.com>
2022-11-09 07:20:19 +0700

    feat: Setup awal re-design halaman web

--- javascript/script_for_sertificate.js
+++
@@ -1,8 +0,0 @@
-const copyrightYear = function () {
-    const year = new Date();
-    return year.getFullYear();
-};
-
-// buat tahun copyright
-const currentYear = document.querySelector('.currrent-year');
-currentYear.innerText = copyrightYear();
