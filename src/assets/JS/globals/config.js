b9be3d2ac7358ff552b2c1838163cf418f15bf4b
tio <sulistiopratomo@gmail.com>
2023-04-08 14:13:09 +0700

    config: menambah dotenv

--- src/assets/JS/globals/config.js
+++ src/assets/JS/globals/config.js
@@ -1,6 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const CONFIG = {
-    FORM_URL_ID:
-        'https://script.google.com/macros/s/AKfycbzU8pAW90MNIJSu2e0leTpLl3_X9RreQrwtFagY5rqfrx6RqG_ZT99oV4v26TNn1D7wMQ/exec',
+    FORM_URL_ID: process.env.FORM_URL_ID,
 };
 
 export { CONFIG };
