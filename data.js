33c1682f2503e51602c7d772e5bbfcf35b4dd003
sulis <sulistiopratomo@gmail.com>
2025-11-26 09:33:10 +0700

    feat: manipulasi card dengan javascript

---
+++ data.js
@@ -0,0 +1,65 @@
+// @ts-check
+
+/**
+  Materi pelajaran yang disajikan berbentuk card
+  @type {Array<{id: number, title: string, desc: string, link: string}>}
+*/
+
+const lectures = [
+  {
+    id: 1,
+    title: "HTML CSS & Git",
+    desc: "Panduan lengkap tentang HTML CSS dan Git",
+    link: "https://html-css-git-tutorial.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Seputar Javascript",
+    desc: "Berisi beberapa bagian teknologi atau library dari Javascript yang berguna dan layak untuk dipelajari.",
+    link: "https://seputar-js.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Belajar Hugo",
+    desc: "Belajar cara menggunakan Hugo static site generator, dari memakai tema yang ada, dan menggunakan tema buatan sendiri dengan go templating",
+    link: "https://tio-pratomo.github.io/learn-hugo/",
+  },
+  {
+    id: 4,
+    title: "Utility JavaScript",
+    desc: "Koleksi dari utility javascript seperti Axios, JSDoc, JSON Server, dan Pino Logger. Itu semua layak dipelajari untuk membantu kita mengembankan webdev lebih luas.",
+    link: "https://tio-pratomo.github.io/utility-js/",
+  },
+  {
+    id: 5,
+    title: "Open API Collection",
+    desc: "Beberapa materi tentang Open API",
+    link: "https://tio-pratomo.github.io/open-api/",
+  },
+  {
+    id: 6,
+    title: "JavaScript Design Pattern",
+    desc: "Kumpulan contoh dan implementasi design pattern dalam JavaScript untuk pembelajaran",
+    link: "https://tio-pratomo.github.io/learn-design-pattern-js/",
+  },
+  {
+    id: 7,
+    title: "Belajar TypeScript",
+    desc: "Eksplorasi TypeScript secara mendalam dengan contoh praktis dan best practices",
+    link: "https://tio-pratomo.github.io/LearnTypescript/",
+  },
+  {
+    id: 8,
+    title: "Info Programming",
+    desc: "Catatan, cheat sheet, dan info-info pemrograman dengan navigasi sederhana untuk dieksplorasi.",
+    link: "https://tio-pratomo.github.io/InfoProgramming/",
+  },
+  {
+    id: 9,
+    title: "Prompt AI (ChatGPT)",
+    desc: "Cara menulis prompt yang tepat agar output yang dihasilkan, sesuai harapan kita",
+    link: "https://tio-pratomo.github.io/PromptAI/",
+  },
+];
+
+export { lectures };
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { lectures } from "./data.js";
+
 // Smooth scrolling for anchor links
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
@@ -22,3 +24,29 @@ window.addEventListener("scroll", function () {
 document
   .getElementById("current-year")
   .insertAdjacentText("beforeend", new Date().getFullYear());
+
+// Insert lecture cards element
+const cards = lectures.map(
+  ({ title, desc, link }) => /* html */ `
+<div class="col-lg-4 col-md-6">
+  <div class="card portfolio-card h-100">
+    <div class="card-body d-flex flex-column">
+      <h5 class="card-title">${title}</h5>
+      <p class="card-text">
+        ${desc}
+      </p>
+      <a
+        href="${link}"
+        class="btn btn-primary mt-auto"
+        target="_blank"
+        >Visit Project</a
+      >
+    </div>
+  </div>
+</div>
+`
+);
+
+cards.forEach((card) => {
+  document.getElementById("mycards").insertAdjacentHTML("beforeend", card);
+});
